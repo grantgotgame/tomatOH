1f14cf8469c7c86b61803915103802d8e70f3f96
GhabrielFratari <ghabrielfratari@gmail.com>
2023-07-07 18:14:15 -0300

    Ball rolling

--- Assets/Scripts/TomatoController.cs
+++ Assets/Scripts/TomatoController.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TomatoController : MonoBehaviour
 {
+    PlayerControl playerControl;
     private Rigidbody playerRb;
+    [SerializeField] float speed;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerControl = new PlayerControl();
+        playerControl.PlayerInputs.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 inputVector = playerControl.PlayerInputs.Movement.ReadValue<Vector2>();
+        playerRb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
     }
 }
