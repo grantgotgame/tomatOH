5cef27237bf40ab05025e8406f8dbcbb62b82956
GhabrielFratari <ghabrielfratari@gmail.com>
2023-07-07 22:46:13 -0300

    small fix

--- Assets/Scripts/TomatoController.cs
+++ Assets/Scripts/TomatoController.cs
@@ -23,7 +23,6 @@ public class TomatoController : MonoBehaviour
     {
         Vector2 inputVector = playerControl.PlayerInputs.Movement.ReadValue<Vector2>();
         playerRb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * dashForce, ForceMode.Impulse);
-        
     }
 
     void FixedUpdate()
