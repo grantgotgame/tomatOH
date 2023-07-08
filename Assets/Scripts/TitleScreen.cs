8bc66ab4aef279f527b1a2f760493d154469264f
Grant Brown <onlythebest@gmail.com>
2023-07-08 16:00:02 -0700

    add start game button

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Start game on button press
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
