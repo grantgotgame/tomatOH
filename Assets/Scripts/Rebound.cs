784c642e84d269dda395382fc5be7f9c8faa35fb
Grant Brown <onlythebest@gmail.com>
2023-07-09 02:58:45 -0700

    add collision sound effect

--- Assets/Scripts/Rebound.cs
+++ Assets/Scripts/Rebound.cs
@@ -7,6 +7,13 @@ public class Rebound : MonoBehaviour
     public float reboundForce = 3f;
     [SerializeField] private ParticleSystem reboundVFX;
 
+    private SoundManager soundManagerScript;
+
+    private void Start()
+    {
+        // Locate sound manager script
+        soundManagerScript = GameObject.Find("Tomato Controller").GetComponent<SoundManager>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -17,7 +24,7 @@ public class Rebound : MonoBehaviour
         otherRigidBody.AddForce(-collision.relativeVelocity * reboundForce, ForceMode.Impulse);
 
         PlayReboundVFX(collision);
-
+        soundManagerScript.PlaySound(soundManagerScript.boing);
     }
 
     public void PlayReboundVFX(Collision collision)
