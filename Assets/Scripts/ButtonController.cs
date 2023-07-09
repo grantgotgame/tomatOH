5247e2dabcf7c91bcc383ab05000a531e7985a8c
Grant Brown <onlythebest@gmail.com>
2023-07-09 09:00:29 -0700

    working intro sequence

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -6,7 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
-    public Button startGameButton;
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+    public GameObject panel4;
+    public GameObject panel5;
+    public GameObject panel6;
+    public GameObject panel7;
+    private int nextCounter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +37,51 @@ public class ButtonController : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("Title Screen");
+    }
+
+    // Load the intro/credits sequence
+    public void StartIntro()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    // Progress through the intro sequence
+    public void Next()
+    {
+        nextCounter += 1;
+        if (nextCounter == 1)
+        {
+            panel1.SetActive(false);
+            panel2.SetActive(true);
+        }
+        else if (nextCounter == 2)
+        {
+            panel2.SetActive(false);
+            panel3.SetActive(true);
+        }
+        else if (nextCounter == 3)
+        {
+            panel3.SetActive(false);
+            panel4.SetActive(true);
+        }
+        else if (nextCounter == 4)
+        {
+            panel4.SetActive(false);
+            panel5.SetActive(true);
+        }
+        else if (nextCounter == 5)
+        {
+            panel5.SetActive(false);
+            panel6.SetActive(true);
+        }
+        else if (nextCounter == 6)
+        {
+            panel6.SetActive(false);
+            panel7.SetActive(true);
+        }
+        else
+        {
+            StartGame();
+        }
     }
 }
